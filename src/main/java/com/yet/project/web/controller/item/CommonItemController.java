c05eca3e59caa70e25dd6edd1db3584d3d9ca705
IK <wndlsrnr1@gmail.com>
2023-06-21 20:10:18 +0900

    [#24] 홈에서 카테고리 구현

---
+++ src/main/java/com/yet/project/web/controller/item/CommonItemController.java
@@ -0,0 +1,40 @@
+package com.yet.project.web.controller.item;
+
+import com.yet.project.domain.item.Category;
+import com.yet.project.domain.item.Subcategory;
+import com.yet.project.domain.service.item.ItemService;
+import com.yet.project.repository.mybatismapper.item.ItemMapper;
+import com.yet.project.web.dto.response.common.APIResponseEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/items")
+@Slf4j
+@RequiredArgsConstructor
+public class CommonItemController {
+
+    private final ItemService itemService;
+    private final ItemMapper itemMapper;
+
+    @GetMapping("/categories")
+    public ResponseEntity requestCategoryAll() {
+        List<Category> categoryListAll = itemService.getCategoryListAll();
+        log.info("categoryList {}", categoryListAll);
+        return APIResponseEntity.success(categoryListAll);
+    }
+
+    @GetMapping("/subcategories")
+    public ResponseEntity requestSubcategoryAll() {
+        Map<Long, List<Subcategory>> subcategoryAllByCategoryId = itemService.getSubcategoryAllByCategoryId();
+        return APIResponseEntity.success(subcategoryAllByCategoryId);
+    }
+
+}
